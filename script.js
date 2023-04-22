1ca9f96a30669b7ee6db971b3a35ce1284887c95
LuxScintilla <catherina.ea@gmail.com>
2023-04-22 21:04:39 +0100

    Add UI for APP

---
+++ script.js
@@ -0,0 +1,19 @@
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+//   this.info = function () {
+//     return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+//   };
+// }
+
+// let theHobbit = new Book(
+//   "The Hobbit",
+//   "J.R.R. Tolkien",
+//   "295 Pages",
+//   "Not read yet"
+// );
+
+// theHobbit.info();
+// console.log(theHobbit.info());
