245c3efd79a565e33fdeb1e8827d79cc994876ae
LuxScintilla <catherina.ea@gmail.com>
2023-04-24 22:41:13 +0100

    Change UI and start JavaScript

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-// function Book(title, author, pages, read) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-//   this.info = function () {
-//     return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
-//   };
-// }
+let inputTitle = document.querySelector("#title");
+let inputAuthor = document.querySelector("#author");
+let displayTitle = document.querySelector(".display-title");
+let displayAuthor = document.querySelector(".display-author");
+const addButton = document.querySelector(".add-button");
+const toggleRead = document.querySelector(".toggle-checkbox");
+const deleteButton = document.querySelector(".delete");
+const bookCard = document.querySelector(".card");
+const libraryContainer = document.querySelector(".library-container");
 
-// let theHobbit = new Book(
-//   "The Hobbit",
-//   "J.R.R. Tolkien",
-//   "295 Pages",
-//   "Not read yet"
-// );
+addButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  addBook();
+  displayBooks();
+});
 
-// theHobbit.info();
-// console.log(theHobbit.info());
+let myLibrary = [];
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function addBook() {
+  let title = inputTitle.value;
+  let author = inputAuthor.value;
+  let newBook = new Book(title, author);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+}
+
+function displayBooks() {
+  for (let book of myLibrary) {
+    let newBook = document.createElement("div");
+    displayTitle.textContent = book.title;
+    displayAuthor.textContent = book.author;
+    newBook.appendChild(displayTitle);
+    newBook.appendChild(displayAuthor);
+    libraryContainer.appendChild(newBook);
+  }
+}
