c98dbc415f491f958340db5f155fa806babd3f16
LuxScintilla <catherina.ea@gmail.com>
2023-04-25 11:02:21 +0100

    Add create card functionality

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let inputTitle = document.querySelector("#title");
 let inputAuthor = document.querySelector("#author");
-let displayTitle = document.querySelector(".display-title");
-let displayAuthor = document.querySelector(".display-author");
 const addButton = document.querySelector(".add-button");
-const toggleRead = document.querySelector(".toggle-checkbox");
+let toggleCheck = document.querySelector("#check");
+let toggleText = document.querySelector(".text-toggle");
 const deleteButton = document.querySelector(".delete");
 const bookCard = document.querySelector(".card");
 const libraryContainer = document.querySelector(".library-container");
 
 addButton.addEventListener("click", function (event) {
   event.preventDefault();
-  addBook();
-  displayBooks();
+  createCard();
+  inputTitle.value = "";
+  inputAuthor.value = "";
 });
 
 let myLibrary = [];
@@ -26,16 +26,45 @@ function addBook() {
   let author = inputAuthor.value;
   let newBook = new Book(title, author);
   myLibrary.push(newBook);
-  console.log(myLibrary);
 }
 
-function displayBooks() {
-  for (let book of myLibrary) {
-    let newBook = document.createElement("div");
-    displayTitle.textContent = book.title;
-    displayAuthor.textContent = book.author;
-    newBook.appendChild(displayTitle);
-    newBook.appendChild(displayAuthor);
-    libraryContainer.appendChild(newBook);
+function createCard() {
+  addBook();
+  let book = myLibrary[myLibrary.length - 1];
+  let code = `
+  <div class="card">
+        <div class="card-title">
+          <p>Title</p>
+          <p class="display-title">${book.title}</p>
+        </div>
+        <div class="card-author">
+          <p>Author</p>
+          <p class="display-author">${book.author}</p>
+        </div>
+        <div class="read">
+          <label class="toggle">
+            <input type="checkbox" id="check" />
+            <span class="slider"></span>
+          </label>
+          <p class="text-toggle">Not Read</p>
+        </div>
+        <button class="delete"><img src="img/delete.svg" /></button>
+      </div>`;
+  libraryContainer.innerHTML += code;
+}
+
+toggleCheck.addEventListener("click", toggleBook);
+
+function toggleBook() {
+  if (toggleCheck.checked) {
+    toggleText.textContent = "Read";
+    toggleText.style.color = "var(--green)";
+  } else if (toggleCheck.checked === false) {
+    toggleText.textContent = "Not Read";
+    toggleText.style.color = "var(--dark-clr)";
   }
 }
+
+deleteButton.addEventListener("click", deleteCard);
+
+function deleteCard() {}
