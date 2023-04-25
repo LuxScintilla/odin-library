0f006451f4cd61e1b95d5e7d1a3ba2da102e7cf5
LuxScintilla <catherina.ea@gmail.com>
2023-04-25 22:10:36 +0100

    Add fully working cards

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 let inputTitle = document.querySelector("#title");
 let inputAuthor = document.querySelector("#author");
 const addButton = document.querySelector(".add-button");
-let toggleCheck = document.querySelector("#check");
-let toggleText = document.querySelector(".text-toggle");
-const deleteButton = document.querySelector(".delete");
-const bookCard = document.querySelector(".card");
+let toggleCheck = document.querySelector(".check");
 const libraryContainer = document.querySelector(".library-container");
 
 addButton.addEventListener("click", function (event) {
@@ -28,6 +25,8 @@ function addBook() {
   myLibrary.push(newBook);
 }
 
+let i = 0;
+
 function createCard() {
   addBook();
   let book = myLibrary[myLibrary.length - 1];
@@ -43,28 +42,42 @@ function createCard() {
         </div>
         <div class="read">
           <label class="toggle">
-            <input type="checkbox" id="check" />
+            <input type="checkbox" id="check-${i}" />
             <span class="slider"></span>
           </label>
-          <p class="text-toggle">Not Read</p>
+          <p class="text-toggle-${i}">Not Read</p>
         </div>
-        <button class="delete"><img src="img/delete.svg" /></button>
+        <button class="delete" id="delete-${i}"><img src="img/delete.svg" /></button>
       </div>`;
-  libraryContainer.innerHTML += code;
+  libraryContainer.insertAdjacentHTML("beforeend", code);
+  document.querySelector("#delete-" + i).addEventListener("click", function () {
+    let removeElement = this.parentNode;
+    libraryContainer.removeChild(removeElement);
+  });
+
+  let toggleText = document.querySelector(".text-toggle-" + i);
+
+  document.querySelector("#check-" + i).addEventListener("click", function () {
+    if (this.checked) {
+      toggleText.textContent = "Read";
+      toggleText.style.color = "var(--green)";
+    } else if (this.checked === false) {
+      toggleText.textContent = "Not Read";
+      toggleText.style.color = "var(--dark-clr)";
+    }
+  });
+
+  i++;
 }
 
-toggleCheck.addEventListener("click", toggleBook);
+// toggleCheck.addEventListener("click", toggleBook);
 
-function toggleBook() {
-  if (toggleCheck.checked) {
-    toggleText.textContent = "Read";
-    toggleText.style.color = "var(--green)";
-  } else if (toggleCheck.checked === false) {
-    toggleText.textContent = "Not Read";
-    toggleText.style.color = "var(--dark-clr)";
-  }
-}
-
-deleteButton.addEventListener("click", deleteCard);
-
-function deleteCard() {}
+// function toggleBook() {
+//   if (toggleCheck.checked) {
+//     toggleText.textContent = "Read";
+//     toggleText.style.color = "var(--green)";
+//   } else if (toggleCheck.checked === false) {
+//     toggleText.textContent = "Not Read";
+//     toggleText.style.color = "var(--dark-clr)";
+//   }
+// }
